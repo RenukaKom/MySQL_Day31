128efe868d1947e91c58f33139d5ba1ca0bab674
unknown <renukakom31@gmail.com>
2022-08-04 22:53:29 +0530

    Created Address_Book Table

---
+++ AddressBook_UC2.sql
@@ -0,0 +1,4 @@
+use Address_Book_Service;
+create table Address_Book (First_Name varchar(30), Last_Name varchar (30), Address varchar (100), City varchar (50), State varchar (30), Zip_Code int (6), Phone_Number int (10), email varchar (30));
+show tables;
+desc Address_Book;
