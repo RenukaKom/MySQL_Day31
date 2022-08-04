d336b41390bf7bf2e6889c481d7030ad82c17444
unknown <renukakom31@gmail.com>
2022-08-04 23:02:56 +0530

    Edit existing contact from Address_Book Table

---
+++ AddressBook_UC4.sql
@@ -0,0 +1,3 @@
+use Address_Book_Service;
+select * from Address_Book;
+UPDATE Address_Book SET Last_Name = 'Shetty', Address = 'Kigga', City = 'Banglore', State = 'Karnataka', Zip_Code = 201451, Phone_Number = '854178230', email = 'kar123@gmail.com' WHERE First_Name = 'Ashmi'; 
