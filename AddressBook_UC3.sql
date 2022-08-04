ffcf182d1407fbe9f8da345b373dc93e3adec21d
unknown <renukakom31@gmail.com>
2022-08-04 22:55:44 +0530

    Insert new Contacts to Address_Book Table

---
+++ AddressBook_UC3.sql
@@ -0,0 +1,10 @@
+show tables;
+desc Address_Book;
+insert into Address_Book values ('Renuka', 'Kom', 'Dahanu', 'Mumbai', 'Maharashtra', 401608, 987456210, 'renuka123@gmail.com');
+select * from Address_Book;
+insert into Address_Book values ('Bhagesh', 'Shelar', 'Virar', 'Mumbai', 'Maharashtra', 405471, 901234544, 'bhagesh123@gmail.com');
+insert into Address_Book values ('Swara', 'Salkar', 'Miramar', 'Panji', 'Goa', 123541, 721234564, 'swara123@gmail.com'),
+('Atul', 'Kathya', 'Bandra', 'Mumbai', 'Maharashtra', 412371, 808034544, 'atulk23@gmail.com'),
+('Ashmi', 'Patel', 'Bordi', 'Vapi', 'Gujrat', 654123, 908050524, 'patel523@gmail.com'),
+('Bhagesh', 'Shelar', 'Virar', 'Mumbai', 'Maharashtra', 405471,901234544, 'bhagesh123@gmail.com');
+select * from Address_Book;
